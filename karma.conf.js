f8cf8154737d21efec04fe4dc5fee17f310c071a
Andrew Larsen <andrew.larse514@gmail.com>
2016-09-08 22:06:19 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -28,11 +28,14 @@ module.exports = function(config) {
     preprocessors: {
     },
 
+    junitReporter : {
+      outputFile: 'test-results.xml'
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters = ['dots', 'junit'],
+    reporters : ['dots', 'junit'],
 
 
     // web server port
